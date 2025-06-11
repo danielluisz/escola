4cd4e5e10531ba6251dc92d659fef57c3ab52f16
danielluis16 <danielluis2513@gmail.com>
2025-06-10 23:06:08 -0300

    trabalhoescola

---
+++ app.py
@@ -0,0 +1,13 @@
+nome = "Daniel" 
+idade = "16" 
+gosto_de = "Programação"
+
+print (f"Meu nome é {nome}")
+print (f"Eu gosto de {gosto_de}")
+
+
+
+
+print (f"Eu tenho {idade} anos")
+idade= "34"
+print (f"{nome} gosta de {gosto_de} e tem {idade} anos.")
